1d6caf55b51669cd1e654c6bd282055319a8e898
Eric Wunderlin <wunde005@umn.edu>
2020-02-19 06:27:54 -0600

    clean up archive.ps1

--- archive.ps1
+++ archive.ps1
@@ -5,10 +5,8 @@ param
     [string]$archiveid=".\archive.id"
 )
 
-#$tag="testtag"
-#$archiveid=".\archive.id"
 try{
-[array]$arc = Import-Csv $archiveid -Header 'Id','Presentation'
+	[array]$arc = Import-Csv $archiveid -Header 'Id','Presentation'
 }
 catch{
    write-host "archive list missing: $archiveid"
@@ -18,12 +16,12 @@ if($alen -lt 1){
 	write-verbose "No Id's found"
 }
 while ($alen -gt 0){
-	$CPreId = $arc[0].Id
+	#$CPreId = $arc[0].Id
 	#write-host Presentations -id `"$($arc[0].Id)`" -tags -post -data (Convertto-json(@{"Tag"="ArchiveDelete"}))
 	$tagged = Presentations -id $($arc[0].Id) -tags -post -data @{"Tag"=$tag}
 	if($tagged.Tag -eq $tag){
-		$arc = $arc | where { $_.Id -ne $arc[0].Id }
-		if($arc -eq $null){
+		$arc = $arc | Where-Object { $_.Id -ne $arc[0].Id }
+		if($null -eq $null){
 			write-host "null"
 		}
 		$alen = $arc.length
@@ -32,7 +30,7 @@ while ($alen -gt 0){
 			clear-content -Path $archiveid 	
 		}
 		else{
-			$arc | convertto-csv -NoTypeInformation | % {$_ -replace '"',''} | Select-Object -Skip 1 | Set-Content -Path $archiveid
+			$arc | convertto-csv -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Select-Object -Skip 1 | Set-Content -Path $archiveid
 		}
 	}
 	else{
@@ -42,13 +40,3 @@ while ($alen -gt 0){
 	[array]$arc = Import-Csv $archiveid -Header 'Id','Presentation'
 	
 }
-#"Id,Presentation" | out-file -filepath $archiveid -Encoding unicode
-#clear-content -Path $archiveid 
-
-<#
-foreach ($a in $arc) { 
-	write-host $a.Id
-	write-host Presentations -id `"$($a.Id)`" -tags -post -data @{"Tag"="ArchiveDelete"}
-
-}
-#>
